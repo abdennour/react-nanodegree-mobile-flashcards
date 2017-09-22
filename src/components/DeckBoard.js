74c9f0bd5bed83ac23ab790216641d9afa3a1445
abdennour <mail@abdennoor.com>
2017-09-22 23:20:49 +0300

    feat: Consume params coming from DecksList to DeckBoard

--- src/components/DeckBoard.js
+++ src/components/DeckBoard.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import { connect } from 'react-redux';
 
 class DeckBoard extends Component {
   render() {
+    const { deck } = this.props.navigation.state.params;
     return (
-      <View>
-        <Text>
-          I am DeckBoard screen :{' '}
-          {JSON.stringify(this.props.navigation.state.params)}
-        </Text>
+      <View style={styles.container}>
+        <View>
+          <Text h1>
+            {deck.name}
+          </Text>
+          <Text h5>
+            {deck.questions.length} cards
+          </Text>
+        </View>
+        <View>
+          <Text>Second part</Text>
+        </View>
+        <View>
+          <Text>3rd part</Text>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  }
+});
+
 export default DeckBoard;
