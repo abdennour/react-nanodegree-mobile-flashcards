19ab549c3a573c16abe84aa498333a897d2eb212
abdennour <mail@abdennoor.com>
2017-09-22 22:27:52 +0300

    feat: Add new deck , submission works successfully!

--- src/components/DeckNew.js
+++ src/components/DeckNew.js
@@ -1,14 +1,70 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { connect } from 'react-redux';
+import {
+  Button,
+  FormLabel,
+  FormInput,
+  FormValidationMessage
+} from 'react-native-elements';
+import { reduxForm, Field } from 'redux-form';
+import { addDeck } from '../actions';
+import { SCREENS } from '../utils/enums';
 
 class DeckNew extends Component {
-  render() {
+  handleSubmit = values => {
+    this.props.addDeck(values.deck);
+    this.props.navigation.navigate(SCREENS.HOME);
+  };
+
+  renderInput = ({ input, meta: { touched, error }, ...rest }) => {
     return (
       <View>
-        <Text>Form Adding Deck</Text>
+        <FormLabel>
+          {rest.label}
+        </FormLabel>
+        <FormInput onChangeText={input.onChange} {...input} />
+        {touched &&
+          error &&
+          <FormValidationMessage>
+            {error}
+          </FormValidationMessage>}
+      </View>
+    );
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Field name="deck" component={this.renderInput} label="Deck Name" />
+        <Button
+          icon={{ name: 'plus', type: 'entypo' }}
+          title="Add"
+          containerViewStyle={{ marginTop: 15 }}
+          onPress={this.props.handleSubmit(this.handleSubmit)}
+        />
       </View>
     );
   }
 }
 
-export default DeckNew;
+function validate(values, ownProps) {
+  const errors = {};
+  if (!values.deck) errors.deck = 'Deck name cannot be empty!';
+  else if (ownProps.decks.find(d => d === values.deck)) {
+    errors.deck = `"${values.deck}" already exists! Add another`;
+  }
+
+  return errors;
+}
+
+function mapStateToProps({ decks }) {
+  return { decks };
+}
+
+export default connect(mapStateToProps, { addDeck })(
+  reduxForm({
+    validate,
+    form: 'newDeck'
+  })(DeckNew)
+);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
 import decks from './decks';
 import questions from './questions';
 
 export default combineReducers({
   decks,
-  questions
+  questions,
+  form
 });
