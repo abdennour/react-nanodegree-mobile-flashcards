b85dbed3a1d77fc470817b2929c6b71dc4cc7087
abdennour <mail@abdennoor.com>
2017-09-23 08:46:05 +0300

    style: remove duplicated key

--- src/utils/enums.js
+++ src/utils/enums.js
@@ -2,7 +2,6 @@ export const SCREENS = {
   HOME: 'Home',
   DECK_NEW: 'DeckNew',
   DECK_BOARD: 'DeckBoard',
-  DECK_NEW: 'DeckNew',
   CARD_NEW: 'CardNew',
   DECK_QUIZ: 'DeckQuiz',
   DECK_SCORE: 'DeckScore'
