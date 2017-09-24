f795f3064cb87d42d1d9c540cc0d555df5464959
abdennour <mail@abdennoor.com>
2017-09-24 13:07:46 +0300

    feat: enable results reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import decks from './decks';
 import questions from './questions';
+import results from './results';
 
 export default combineReducers({
   decks,
   questions,
+  results,
   form
 });
