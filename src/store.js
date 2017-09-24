b28a0bce4281a1f76916ad31e6bab0b665ddb002
abdennour <mail@abdennoor.com>
2017-09-25 00:44:36 +0300

    feat: Persist data on the local storage of device 🎉 😎

--- src/store.js
+++ src/store.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
+import { persistStore, autoRehydrate } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
+
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const store = createStore(reducers, {}, compose(autoRehydrate()));
+
+persistStore(store, { storage: AsyncStorage });
 
 export default store;
